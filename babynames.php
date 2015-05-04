82021c7d1799918121f12a4811169f3d0f893f04
killazilla54 <killazilla@s172-20-29-h231.paws.uga.edu>
2015-05-03 21:47:07 -0400

    Current build

--- babynames.php
+++ babynames.php
@@ -1,24 +1,37 @@
 <?php
 $name = $_POST["babyname"];
-$name = strtoupper($name);
+
 $type = $_POST["type"];
-if($type == "meaning"){
+
+if($type == "list"){
+	Header('Content-type: text/html');
+    //header("Content-Type: text/plain");
+    $returnStr = "";
+    $lines = file_get_contents("list.txt");
+    $rows = explode("\n", $lines);
+    foreach($rows as $row){
+        $returnStr .= "<option value=\"".$row."\" >".$row."</option>";
+    }
+    echo $returnStr;
+}
+
+else if($type == "meaning"){
+	Header('Content-type: text/html');
+	$name = strtoupper($name);
 	$txt_file = file_get_contents('meanings.txt');
 	$rows = explode("\n", $txt_file);	
 	$found = false;
 	$returnStr = "";
 	foreach($rows as $row){
-		//echo $row;
-		//Name found, end loop
+
 		if($found){
 			break;
 		}
 
 		$content = explode(" ", $row);
-		//echo $content[0] ." : ";
+
 		if($content[0]==$name){
 			$returnStr .= "<div><p>The name <strong>".$name."</strong> means...</p><hr/><p><q>";
-//			echo "here!!!!: ".$content->count();
 			for($i = 1; $i < sizeof($content); $i++){
 				$returnStr .= $content[$i]." ";
 			}
@@ -27,6 +40,73 @@ if($type == "meaning"){
 		}
 			
 	}
+	if($found == false){
+		$returnStr = "There is no meaning for the name <strong>".$name."</strong>";	
+	}
 	echo $returnStr;
+}
+else if ($type == "rank") {
+     $gender = $_POST["gender"];
+     $txt_file = file_get_contents('rank.txt');
+     $rows = explode("\n", $txt_file);
+     $found2 = false;
+     $xml = $name;
+     $returnStr = $name;
+     foreach($rows as $row) {
+
+          if ($found2) {
+               break;
+          }
+
+          $content = explode(" ", $row);
+          if (strcmp($content[0],$name) == 0 && strcmp($content[1],$gender) == 0) {
+          
+               $xml = new SimpleXMLElement("<baby></baby>");
+               $xml->addAttribute('name',$name);
+               $xml->addAttribute('gender',$gender);
+               $year = 1900;
+               if (sizeof($content) == 15)
+                 $year = 1890;
+               for ($i = 2; $i < sizeof($content); $i++) {
+                 $rank = $xml->addChild('rank',$content[$i]);
+                 $rank->addAttribute('year',$year);
+                 $year += 10;
+               }//end for loop
+
+               $found2 = true;
+          }//end if
+
+     }//end foreach
+     Header('Content-type: text/xml');
+     echo $xml->asXML();
+	 
+
+}//end rank/popularity
+
+else{
+	Header('Content-type: text/html');
+	$name = $_GET['name'];
+	$gender = $_GET['gender'];
+	$type = $_GET['type'];
+	$actors = '{"actors":[';
+	
+	$user = 'root';
+	$pass = 'kaiden90';
+	$dbh = new PDO('mysql:host=localhost;dbname=imdb', $user, $pass);
+	
+	if($type == "celebs") {
+	//Gets the searched actor's id
+		$q1 = "SELECT first_name, last_name, film_count
+				FROM actors
+				WHERE ((first_name LIKE '" . $name . " %' OR first_name = '" . $name . "')
+					AND gender = '" . $gender . "') ORDER BY film_count DESC";
+		foreach ($dbh->query($q1) as $row) {
+			$actors .= '{"firstName": "' . $row[0] . '", "lastName": "' . $row[1] . '", "filmCount": "' . $row[2] . '"},';
+		}
+		$dbh = null;
+		$actors = substr($actors, 0, strlen($actors)-1);
+		$actors .= "]}";
+		echo $actors;
+	}
 }
 ?>
--- names.js
+++ names.js
@@ -1,4 +1,15 @@
-// JavaScript Document
+Event.observe(window, "load", function(){
+	getList();
+   $("search").observe("click", searchAll);
+});
+
+function searchAll(){
+	var name = document.getElementById("allnames").value;
+	getMeaning(name);
+	getRank(name);
+	findCelebs(name);	
+	
+}
 
 function getMeaning(name){
 	document.getElementById('resultsarea').style.display = 'block';
@@ -9,16 +20,7 @@ function getMeaning(name){
 			type: 'meaning',
   		},
 		onSuccess: successFunc
-  });
-	/*
-	$.ajax({
-		type:"POST",
-		url:"babynames.php",
-		data:{babyname:name,type:"meaning"},
-		success: function(data){
-			$("#meaning").html(data);
-		}
-	});*/
+  	});
 }
 
 function successFunc(response){
@@ -27,3 +29,98 @@ function successFunc(response){
 	
 	document.getElementById("meaning").innerHTML = response.responseText;
 }
+
+//JOSEPH
+function findCelebs(name){
+	document.getElementById('resultsarea').style.display = 'block';
+    var url = "babynames.php?type=celebs&name=";
+    //url += $("allnames").value;
+    url += name;
+    url += "&gender=";
+    var gender = "m";
+    if(!$("genderm").checked){
+        gender = "f";
+    }
+    url += gender;
+    new Ajax.Request(url, {
+        method: 'get',
+        onSuccess: showCelebs
+    });
+}
+
+function showCelebs(ajax) {
+	document.getElementById('loadingcelebs').style.display = 'none';
+
+
+    var data = JSON.parse(ajax.responseText);
+	console.log("PARSED!");
+    var celebList = document.getElementById("celebs");
+    while (celebList.firstChild) {
+        celebList.removeChild(celebList.firstChild);
+    }
+    for (var i = 0; i < data.actors.length; i++) {
+        var li = document.createElement("li");
+        li.innerHTML = data.actors[i].firstName + " " + data.actors[i].lastName + " (" +
+        data.actors[i].filmCount + ")";
+        celebList.appendChild(li);
+    }
+    
+}
+
+function getList(){
+    new Ajax.Request('babynames.php', {
+        method: 'post',
+        parameters: {type: 'list'},
+        onSuccess: successList
+    });
+}
+
+function successList(response){
+    document.getElementById("allnames").disabled = false;
+    document.getElementById("allnames").innerHTML=response.responseText;
+}
+
+function rankSuccess(response) {
+    document.getElementById('loadinggraph').style.display = 'none';
+   
+    //console.log(response.responseText);
+    var xmlDoc = response.responseXML;
+    console.log(xmlDoc);
+
+    var ranks = xmlDoc.getElementsByTagName("rank");
+    var txt = "";
+
+    txt += "<tr>";
+    for (i=0; i < ranks.length;i++) {
+        txt += "<th>" + ranks[i].getAttribute('year') + "</th>";
+    }//end for loop
+
+    txt += "</tr><tr>";
+
+    for (i=0; i < ranks.length;i++) {
+        txt += "<td><div>" + ranks[i].childNodes[0].nodeValue + "</div></td>";
+    }//end for loop
+    txt += "</tr>";
+
+    document.getElementById('graph').innerHTML = txt;
+}//end rankSuccess
+
+function getRank(name) {
+//    var dropdown = document.getElementById('allnames');
+ //   var name = dropdown.options[dropdown.selectedIndex].text;
+    var gender = "";
+    if (document.getElementById('genderm').checked)
+        gender = "m";
+    else
+        gender = "f";
+    document.getElementById('resultsarea').style.display = 'block';
+    new Ajax.Request('babynames.php', {
+            method: 'post',
+            parameters: {
+                babyname: name,
+                gender: gender,
+                type: 'rank',
+            },
+            onSuccess: rankSuccess
+    });
+}//end getRank
